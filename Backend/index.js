4351807583f45d3a6d2aec916f18b8faf5a520ed
NikosGour <nikosgournak@gmail.com>
2022-05-14 23:08:28 +0300

    Latest

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,66 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+	let debug = {};
 	let response = {};
+	headers = {
+		"Access-Control-Allow-Origin": "*",
+		"Content-Type": "application/json",
+	};
 
-	switch (req.url) {
-		case "/api":
-			if (req.method === "GET") {
-				res.writeHead(200, { "Content-Type": "application/json" });
-				response = { body: "Success" };
-				res.end(JSON.stringify(response));
-			} else if (req.method === "POST") {
-				res.writeHead(300);
-				res.end("oxi");
-			}
-			break;
+	debug.url = req.url;
+	debug.method = req.method;
 
-		default:
-			res.writeHead(404, { "Content-Type": "application/json" });
-			response = { body: "Not found" };
+	let match = req.url.match(/api\/(\w+)?/);
+	if (req.url === "/api") {
+		if (req.method === "GET") {
+			res.writeHead(200, headers);
+			response = { body: "Success" };
 			res.end(JSON.stringify(response));
-			break;
+		} else if (req.method === "POST") {
+			response = { body: "Book added successfully" };
+
+			let body = "";
+			req.on("data", (data) => {
+				body += data;
+			});
+
+			req.on("end", () => {
+				headers["Access-Control-Allow-Methods"] = "GET,POST";
+				res.writeHead(200, headers);
+				res.end(JSON.stringify(response));
+			});
+
+			if (body === "") {
+				await setTimeout(() => 0, 500);
+			}
+
+			debug.request_body = JSON.parse(body);
+		} else if (req.method === "OPTIONS") {
+			headers["Access-Control-Allow-Headers"] = "*";
+			res.writeHead(200, headers);
+			response = { body: "Options Success" };
+			res.end(JSON.stringify(response));
+		}
+	} else if (match != null) {
+		res.writeHead(404, headers);
+		response = { body: `Not ${match[1]}` };
+		res.end(JSON.stringify(response));
+	} else {
+		res.writeHead(404, headers);
+		response = { body: "Not found" };
+		res.end(JSON.stringify(response));
 	}
-	console.log("Hello World");
-	console.log(response);
+
+	debug.response = response;
+	console.log(`${JSON.stringify(debug, null, "\t")}`);
+	console.log("-".repeat(100));
 });
 
-const PORT = 5050;
+const PORT = 8080;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => {
+	console.log("-".repeat(100));
+	console.log(`Server running on port ${PORT}`);
+	console.log("-".repeat(100));
+});

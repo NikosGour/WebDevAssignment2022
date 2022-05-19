1abe8a2fb88004035525dcbd56a557d6495921af
NikosGour <nikosgournak@gmail.com>
2022-05-20 02:12:37 +0300

    Latest

--- Backend/index.js
+++ Backend/index.js
@@ -1,81 +1,46 @@
-// const http = require("http");
-
-// const server = http.createServer(async (req, res) => {
-// 	let debug = {};
-// 	let response = {};
-// 	headers = {
-// 		"Access-Control-Allow-Origin": "*",
-// 		"Content-Type": "application/json",
-// 	};
-
-// 	debug.url = req.url;
-// 	debug.method = req.method;
-
-// 	let match = req.url.match(/api\/(.+)?/);
-// 	if (req.url === "/api") {
-// 		if (req.method === "GET") {
-// 			res.writeHead(200, headers);
-// 			response = { body: "Success" };
-// 			res.end(JSON.stringify(response));
-// 		} else if (req.method === "POST") {
-// 			response = { body: "Book added successfully" };
-
-// 			let body = "";
-// 			req.on("data", (data) => {
-// 				body += data;
-// 			});
-
-// 			req.on("end", () => {
-// 				headers["Access-Control-Allow-Methods"] = "GET,POST";
-// 				res.writeHead(200, headers);
-// 				res.end(JSON.stringify(response));
-// 			});
-
-// 			if (body === "") {
-// 				await setTimeout(() => 0, 500);
-// 			}
-
-// 			debug.request_body = JSON.parse(body);
-// 		} else if (req.method === "OPTIONS") {
-// 			headers["Access-Control-Allow-Headers"] = "*";
-// 			res.writeHead(200, headers);
-// 			response = { body: "Options Success" };
-// 			res.end(JSON.stringify(response));
-// 		}
-// 	} else if (match != null) {
-// 		let utf_decoded_body = decodeURIComponent(match[1]);
-// 		res.writeHead(404, headers);
-// 		response = { body: `Not ${utf_decoded_body}` };
-// 		res.end(JSON.stringify(response));
-// 	} else {
-// 		res.writeHead(404, headers);
-// 		response = { body: "Not found" };
-// 		res.end(JSON.stringify(response));
-// 	}
-
-// 	debug.response = response;
-// 	console.log(`${JSON.stringify(debug, null, "\t")}`);
-// 	console.log("-".repeat(100));
-// });
-
-// const { PORT, URL } = require("./setting.json");
-
-// server.listen(PORT, () => {
-// 	console.log("-".repeat(100));
-// 	console.log(`Server running on port ${PORT} , http://localhost:${PORT}/api`);
-// 	console.log("-".repeat(100));
-// });
-
-
-
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
 const app = express();
+
+const connection = mysql.createConnection({
+	host: "localhost",
+	user: "user",
+	password: "0101",
+	database: "webdev"
+});
+
 
 const PORT = 8080;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/api", (req, res) => {
 
-	res.send("Hello World");
+	res.send("Testing")
+});
+
+app.post("/api", async (req, res) => {
+	const book = req.body;
+	console.log(book);
+
+	connection.connect((err) => {
+		if (err) {
+			console.log(err);
+			res.status(500).send(err);
+		}
+		console.log("Connected to database");
+
+	});
+	console.log("after connection");
+});
+
+app.get("/api/:text", (req, res) => {
+	let str = decodeURIComponent(req.params.text);
+	console.log(str);
+	res.end(str);
 });
 
 app.listen(PORT, () => {
